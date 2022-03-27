5f2162e10f2657169c77791e1b33e6a444ea1ea5
jsureka <bsse1115@iit.du.ac.bd>
2022-03-28 03:08:58 +0600

    Initial Commit

---
+++ src/main/java/Location.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import java.util.Random;
+
+public enum Location {
+    DEFAULT, USA, ASIA;
+
+    public static Location generateRandomLocation() {
+        Location[] values = Location.values();
+        int length = values.length;
+        int randIndex = new Random().nextInt(length);
+        return values[randIndex];
+    }
+}
